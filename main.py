305bec021f8a94d4c8af9c406126fec3fefaac06
Mr.X <79870712+n30mrx@users.noreply.github.com>
2023-09-10 20:39:27 +0300

    small change

--- main.py
+++ main.py
@@ -11,29 +11,32 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>."""
 import requests
-import telebot
+import json
+import telebot, os
 from telebot import types
 
-bot = telebot.TeleBot("TOKEN")
+bot = telebot.TeleBot("6303392772:AAH_S0SMHoN5xt3vFyCEb728mwuSm8UgUDc")
 devUrl = types.InlineKeyboardButton(text="Mr. X - المطور",  url="https://t.me/linux_nerd")
 foss = "This bot is foss, you can find the source code on https://github.com/n30mrx/archwikibot"
 
 @bot.message_handler(commands=['start'])
 def start(message):
     myKey  = types.InlineKeyboardMarkup(row_width=3)
+    useBot = types.InlineKeyboardButton(text="Use bot - استعمل البوت", switch_inline_query_chosen_chat="")
     myKey.add(devUrl)
-    bot.send_message(message.chat.id,"Hey! this is an inline bot used to search through the arch wiki :)\njust mention the bot and type your query, then choose a resault!", reply_markup=myKey)
+    bot.send_message(message.chat.id,"Hey! this is an inline bot used to search through the arch wiki :)\njust mention the bot and type your query, then choose a result!", reply_markup=myKey)
     
 
 @bot.inline_handler(func=lambda query: len(query.query)>0)
 def inline(inlineQuery):
     req = requests.get(f"https://wiki.archlinux.org/api.php?action=opensearch&search={inlineQuery.query}&limit=50&uselang=en")
+    print(req.text)
     resultsT = req.json()[1]
     resultsL = req.json()[3]
     c = 0
     results = []
     for i in resultsT:
-        results.append(types.InlineQueryResultArticle(id=i, title=i, input_message_content=types.InputTextMessageContent(disable_web_page_preview=False, message_text=f"{i}\n{resultsL[c]}")))
+        results.append(types.InlineQueryResultArticle(id=c, title=i, input_message_content=types.InputTextMessageContent(disable_web_page_preview=False, message_text=f"{i}\n{resultsL[c]}")))
         c+=1
     bot.answer_inline_query(inlineQuery.id,results=results)
 
